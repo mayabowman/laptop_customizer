cdedd787b902dfaf2795e9ba2e350018c7799a23
Maya Bowman <mayashady1@gmail.com>
2019-09-10 09:03:53 -0400

    worked on feature name error

--- src/Summary.js
+++ src/Summary.js
@@ -7,14 +7,16 @@ const USCurrencyFormat = new Intl.NumberFormat('en-US', {
 
 class Summary extends Component {
   render() {
+    
     return (
       Object.keys(this.props.selected).map((feature, idx) => {
+        console.log(feature);
         const featureHash = feature + '-' + idx;
         const selectedOption = this.props.selected[feature];
   
         return (
           <div className="summary__option" key={featureHash}>
-            <div className="summary__option__label">{feature} </div>
+            <div className="summary__option__label">{feature}</div>
             <div className="summary__option__value">{selectedOption.name}</div>
             <div className="summary__option__cost">
               {USCurrencyFormat.format(selectedOption.cost)}
